c6eb1c73d6e961b2afe332a13bf1dcc303ef30c1
sadanandkaji <sadanandkaji1@gmail.com>
2025-01-31 14:12:20 +0530

    added required apis

--- tests/SQL/count_assignments_in_each_grade.sql
+++ tests/SQL/count_assignments_in_each_grade.sql
@@ -1,1 +1,8 @@
 -- Write query to get count of assignments in each grade
+SELECT
+    grade,
+    COUNT(*) AS assignment_count
+FROM
+    assignments
+GROUP BY
+    grade;
--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,28 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+WITH TeacherGradingCount AS (
+    SELECT
+        teacher_id,
+        COUNT(*) AS total_graded
+    FROM
+        assignments
+    WHERE
+        grade IS NOT NULL
+    GROUP BY
+        teacher_id
+),
+MaxGradingTeacher AS (
+    SELECT
+        teacher_id
+    FROM
+        TeacherGradingCount
+    ORDER BY
+        total_graded DESC
+    LIMIT 1
+)
+SELECT
+    COUNT(*) AS grade_a_count
+FROM
+    assignments
+WHERE
+    grade = 'A'
+    AND teacher_id = (SELECT teacher_id FROM MaxGradingTeacher);
